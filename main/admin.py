73ff85813a1bd06edba219388556be6fd1646f98
M. Reda <mohamedreda11@gmail.com>
2024-08-12 17:43:18 +0300

    Added several clarifying comments

--- main/admin.py
+++ main/admin.py
@@ -2,12 +2,14 @@ from django.contrib import admin
 from . import models
 
 # Register your models here.
-#-----
+#Adding website parts to the admin panel side
 
+#display the 'alt_text' and the image thumbnail ('image_tag') for each banner
 class banneradmin(admin.ModelAdmin):
     list_display=('alt_text','image_tag')  
 admin.site.register(models.banners,banneradmin)
 
+#display the 'title' and the image thumbnail ('image_tag') for each service
 class serviceadmin(admin.ModelAdmin):
     list_display=('title','image_tag')  
 admin.site.register(models.service,serviceadmin)
--- main/models.py
+++ main/models.py
@@ -5,11 +5,13 @@ from django.utils.html import mark_safe
 class banners(models.Model):
     img=models.ImageField(upload_to="banners/")
     alt_text=models.CharField(max_length=150)
-
+    #Names images after their alt text
     def __str__(self):
         return self.alt_text
     
     def image_tag(self):
+    # Returns an HTML <img> tag to display the product's image in the admin interface
+    # The image is displayed as a thumbnail with a width of 80 pixels.
         return mark_safe('<img src="%s" width="80"/>' % (self.img.url))
 
 
